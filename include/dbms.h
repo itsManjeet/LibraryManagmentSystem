e62ca4652912f6c4a0916cb2dbb341c23b243164
itsmanjeet <sainishaab550@gmail.com>
2019-05-09 10:16:53 +0530

    add function to acess data

--- include/dbms.h
+++ include/dbms.h
@@ -1,11 +1,8 @@
 #ifndef DBMS_H
 #define DBMS_H
 
-int
-add_data (char* data,
-          int   roll,
-          char* name,
-          int*  bbk,
-          int*  mob);
+int add_data (char* data, int   roll, char* name, int*  bbk, int*  mob);
 
+
+int get_data (char* data, char* colname, char* ref, void (*callback)(), void* indata, char *msg);
 #endif
